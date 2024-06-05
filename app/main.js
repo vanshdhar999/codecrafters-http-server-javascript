3d9626f69e62a7f911dd1465fcc0882a3cf2e27c
Vansh Dhar <11avanshdhar@gmail.com>
2024-06-05 16:25:12 +0530

    pass

--- app/main.js
+++ app/main.js
@@ -6,9 +6,9 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
-    const request = data.toString();
+    const request = data.toString().split(" ");
     console.log(request);
-    if(request.startsWith('GET /')){
+    if(request[0] === 'GET' && request[1] === '/'){
         socket.write("HTTP/1.1 200 OK\r\n\r\n");
     }
     else{
