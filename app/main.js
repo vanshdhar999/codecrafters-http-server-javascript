a0ecea05b5e67ddc04751308469b67de25edff14
Vansh Dhar <11avanshdhar@gmail.com>
2024-06-05 15:48:10 +0530

    pass stge

--- app/main.js
+++ app/main.js
@@ -5,7 +5,10 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+if(typeof(URL) === String){
+    socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+}
+socket.write("HTTP/1.1 200 OK\r\n\r\n");
   socket.on("close", () => {
     socket.end();
     server.close();
