de4a3e155bbcf63e36a84bdff64ac7ce7ffffae2
Vansh Dhar <11avanshdhar@gmail.com>
2024-06-05 16:08:50 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -7,7 +7,7 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
-    if(request.startsWith('GET / ')){
+    if(request.startsWith('GET /')){
         socket.write("HTTP/1.1 200 OK\r\n\r\n");
     }
     else{
