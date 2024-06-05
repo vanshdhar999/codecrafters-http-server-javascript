5c1fa1e955c0b621d07e78f67b143e4656275fcb
Vansh Dhar <11avanshdhar@gmail.com>
2024-06-05 16:11:26 +0530

    pass

--- app/main.js
+++ app/main.js
@@ -5,17 +5,20 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("data", (data) => {
+  socket.on('data', (data) => {
     const request = data.toString();
     if(request.startsWith('GET /')){
         socket.write("HTTP/1.1 200 OK\r\n\r\n");
     }
     else{
-        socket.write("HTTP/1.1 404 NOT FOUND\r\n\r\n")
+        socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
     }
 
   });
-  socket.end();
+  socket.on('close', ()=>{
+    socket.end();
+    server.close();
+  })
 });
 
 server.listen(4221, "localhost");
